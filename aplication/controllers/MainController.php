87b93c7c29b3fa15dca7cde31b2b890f2a137e2a
Ziyodulla Ibragimov <ziyodjon@gmail.com>
2023-03-09 21:08:48 +0500

    Added Main and News controllers

---
+++ aplication/controllers/MainController.php
@@ -0,0 +1,9 @@
+<?php
+namespace aplication\controllers;
+
+use aplication\core\Controller;
+class MainController extends Controller{
+    public function testAction(){
+        echo 'Hello from Main controller';
+    }
+}
---
+++ aplication/controllers/NewsController.php
@@ -0,0 +1,9 @@
+<?php
+namespace aplication\controllers;
+
+use aplication\core\Controller;
+class NewsController extends Controller{
+    public function showAction(){
+        echo "Showing news lists";
+    }
+}
