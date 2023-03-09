dbaee8eaa7cb5f4102b64c4d14b31307a2c294bb
Ziyodulla Ibragimov <ziyodjon@gmail.com>
2023-03-09 21:42:22 +0500

    last changes

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello, this is index.php";
+require "aplication/lib/Dev.php";
+//use aplication\core\Router;
+
+// If class file exists Autoload all classes here
+spl_autoload_register(function ($class) {
+    $path = str_replace('\\','/',$class.".php");
+    if(file_exists($path)){
+        include $path;
+    }
+});
+
+$router = new aplication\core\Router();
+
+$router->run();
