89ac011b4aa73a9b0f8868e551271b9e279e263c
Ziyodulla Ibragimov <ziyodjon@gmail.com>
2023-03-09 21:10:00 +0500

    Added routes arrays to routes.php

---
+++ aplication/config/routes.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '' => [
+        'controller' => 'main',
+        'action' => 'test',
+    ],
+
+    'admin/logins' => [
+        'controller' => 'admin',
+        'action' => 'logins',
+    ],
+
+    'admin/register' => [
+        'controller' => 'admin',
+        'action' => 'register',
+    ],
+
+    'news/show' => [
+    'controller' => 'news',
+        'action' => 'show',
+    ]
+];
