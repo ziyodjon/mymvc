acb25a4460b369835e1522e11ea0a13ef8543e83
Ziyodulla Ibragimov <ziyodjon@gmail.com>
2023-03-12 20:06:19 +0500

    Added main abstract Model

---
+++ application/core/Model.php
@@ -0,0 +1,12 @@
+<?php
+namespace application\core;
+
+use application\lib\Db;
+
+abstract class Model{
+    public $db;
+
+    public function __construct(){
+        $this->db = new Db();
+    }
+}
