7a79b72eda1c11791bdce946dab5db91934ee1c4
Ziyodulla Ibragimov <ziyodjon@gmail.com>
2023-03-13 09:13:11 +0500

    login page changed

--- application/views/account/login.php
+++ application/views/account/login.php
@@ -1,1 +1,11 @@
-<p>Login page</p>
+<h2>Login page</h2>
+
+<form action="/account/login/" method="post">
+    <p>Login</p>
+    <input type="text" name="login">
+    <p>Password</p>
+    <input type="password" name="password">
+    <br>
+    <br>
+    <input type="submit" name="enter">
+</form>
